83c65d06cf4d822e54890912392803408558af66
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-14 17:19:21 +0000

    add code to increment score and incorrect answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("great job!! :D");
+        incrementScore();
     } else {
         alert(`damn, you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -66,9 +68,20 @@ function calculateCorrectAnswer() {
         throw `unimplemented operator ${operator}. Aborting!`
     }
 }
-
+/**
+ * Gets the current score from DOM and increments it by 1
+ */
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText)
+    document.getElementById("score").innerText = ++oldScore
+}
 
+/**
+ * Gets the current incorrect answer from DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText)
+    document.getElementById("incorrect").innerText = ++oldScore
 }
 
 function displayAdditionalQuestions (operand1, operand2) {
