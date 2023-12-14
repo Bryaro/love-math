20932a63085d104657ccde824cf75fd8ce3042c2
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-14 13:40:18 +0000

    display addition question for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,27 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("submit button clicked")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert("you clicked ${gameType}")
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame() {
+/**
+ * The function runGame "loop" called
+ */
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionalQuestions(num1, num2)
+    } else {
+        alert(`not known game type:${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -30,8 +43,10 @@ function incrementScore() {
 
 }
 
-function displayAdditionalQuestions () {
-    
+function displayAdditionalQuestions (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestions () {
