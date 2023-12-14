794b8802f96810225cbf69173de9b69c0715931d
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-14 12:55:21 +0000

    add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-function runGmae() {
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            //"this" refers to that button user clicked
+            //for instance if we click submit button then "this" refers to sumbit button
+            if (this.getAttribute("data-type") === "submit") {
+                alert("submit button clicked")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert("you clicked ${gameType}")
+            }
+        })
+    }
+})
+
+function runGame() {
 
 }
 
